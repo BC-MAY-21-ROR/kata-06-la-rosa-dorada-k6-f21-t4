4d974beafaf3c54f93d1db4d9e32c6a54c942bc7
German Ruiz <43076503+germanruzca@users.noreply.github.com>
2022-03-03 17:22:49 -0600

    Program finished.
    The complete program was refactorized from the original. Plus the first gems were added to the Gemfile.
    
    Co-authored-by: AlejandroCuevasZepeda <acuevas@ucol.mx>
    Co-authored-by: Nallely Garcia  <nallely64@users.noreply.github.com>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -2,92 +2,84 @@
 
 # class GildedRose
 class GildedRose
-  
   def initialize(items)
     @items = items
   end
-  
-  def update_quality()
+
+  def update_quality
     @items.each do |item|
-      item.sell_in -=1 if item.name != 'Sulfuras, Hand of Ragnaros' && item.sell_in > 0
-      case item.name
-      when 'Aged Brie'
-        if item.sell_in == 0
-          increment_double_quality(item)
-        else
-          increment_quality(item)
-        end
-      when 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in
-        item.quality = item.quality
-      when 'Backstage passes to a TAFKAL80ETC concert'
-        if item.sell_in == 0
-          item.quality = 0
-        elsif item.sell_in <= 5
-          five_or_less(item)
-        elsif item.sell_in <= 10
-          ten_or_less(item)
-        else 
-          item.quality +=1
-        end
-      when 'Conjured Mana Cake'
-        decrement_double_quality(item)
-      else
-        if item.quality.positive?
-          if item.sell_in.negative? && item.quality > 1
-            decrement_double_quality(item)
-          elsif item.sell_in.negative? && item.quality == 1
-            decrement_quality(item)
-          else 
-            # decrement_qualitys
-            decrement_quality(item)
-            decrement_sell_in(item)
-          end
-        end
-      end
-    end  
+      item.sell_in -= 1 if item.name != 'Sulfuras, Hand of Ragnaros' && item.sell_in.positive?
+      items_case(item)
+    end
   end
 
-  def decrement_double_quality(item)
-    item.quality -= 2
+  def items_case(item)
+    case item.name
+    when 'Aged Brie' then cheese(item)
+    when 'Sulfuras, Hand of Ragnaros' then sulfuras(item)
+    when 'Backstage passes to a TAFKAL80ETC concert' then backstage(item)
+    when 'Conjured Mana Cake' then decrement_double_quality(item)
+    else
+      item.quality -= item.sell_in.positive? ? 1 : 2
+      min_quality_limit(item)
+    end
+  end
+
+  def cheese(item)
+    item.quality += item.sell_in.positive? ? 1 : 2
+    item.quality = 50 if item.quality > 50
+  end
+
+  def sulfuras(item)
+    item.sell_in = item.sell_in
+    item.quality = item.quality
+  end
+
+  def backstage(item)
+    if item.sell_in.zero?
+      item.quality = 0
+    elsif item.sell_in <= 10
+      less_ten_or_five(item)
+    else
+      item.quality += 1
+    end
+  end
+
+  def less_ten_or_five(item)
+    item.quality += item.sell_in <= 5 ? 3 : 2
+    max_quality_limit(item)
+  end
+
+  def max_quality_limit(item)
+    item.quality = 50 if item.quality > 50
+  end
+
+  def min_quality_limit(item)
     item.quality = 0 if item.quality.negative?
   end
 
   def increment_quality(item)
     item.quality += 1
-    item.quality = 50 if item.quality >= 50
+    max_quality_limit(item)
   end
 
   def increment_double_quality(item)
     item.quality += 2
-    item.quality = 50 if item.quality >= 50
+    max_quality_limit(item)
   end
-  
+
   def decrement_quality(item)
     item.quality -= 1
+    min_quality_limit(item)
   end
 
   def decrement_double_quality(item)
     item.quality -= 2
-    item.quality = 0 if item.quality.negative?
-  end
-
-  def decrement_sell_in(item)
-    item.sell_in -= 1
-    item.sell_in = 0 if item.sell_in.negative?
-  end
-
-  def ten_or_less(item)
-    item.quality += 2
-    item.quality = 50 if item.quality > 50
-  end
-
-  def five_or_less(item)
-    item.quality += 3
-    item.quality = 50 if item.quality > 50
+    min_quality_limit(item)
   end
 end
 
+# Class item
 class Item
   attr_accessor :name, :sell_in, :quality
 
@@ -97,7 +89,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
