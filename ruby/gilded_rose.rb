5da2aa8aa613de94dd1d292048e8c953d6c3becf
German Ruiz <43076503+germanruzca@users.noreply.github.com>
2022-03-02 18:00:07 -0600

    New methods implementation
    The new methods were implemented in the GildedRose class
    
    Co-authored-by: AlejandroCuevasZepeda <acuevas@ucol.mx>
    Co-authored-by: Nallely Garcia  <nallely64@users.noreply.github.com>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -2,57 +2,89 @@
 
 # class GildedRose
 class GildedRose
-
+  
   def initialize(items)
     @items = items
   end
-
+  
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality -= 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in -= 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality -= 1
-              end
-            end
-          else
-            item.quality = 0
-          end
+      item.sell_in -=1 if item.name != 'Sulfuras, Hand of Ragnaros' && item.sell_in > 0
+      case item.name
+      when 'Aged Brie'
+        if item.sell_in == 0
+          increment_double_quality(item)
         else
-          if item.quality < 50
-            item.quality += 1
+          increment_quality(item)
+        end
+      when 'Sulfuras, Hand of Ragnaros'
+        item.sell_in = item.sell_in
+        item.quality = item.quality
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in == 0
+          item.quality = 0
+        elsif item.sell_in <= 5
+          five_or_less(item)
+        elsif item.sell_in <= 10
+          ten_or_less(item)
+        else 
+          item.quality +=1
+        end
+      when 'Conjured Mana Cake'
+        decrement_double_quality(item)
+      else
+        if item.quality.positive?
+          if item.sell_in.negative? && item.quality > 1
+            decrement_double_quality(item)
+          elsif item.sell_in.negative? && item.quality == 1
+            decrement_quality(item)
+          else 
+            # decrement_qualitys
+            decrement_quality(item)
+            decrement_sell_in(item)
           end
         end
       end
-    end
+    end  
+  end
+
+  def decrement_double_quality(item)
+    item.quality -= 2
+    item.quality = 0 if item.quality.negative?
+  end
+
+  def increment_quality(item)
+    item.quality += 1
+    item.quality = 50 if item.quality >= 50
+  end
+
+  def increment_double_quality(item)
+    item.quality += 2
+    item.quality = 50 if item.quality >= 50
+  end
+  
+  def decrement_quality(item)
+    item.quality -= 1
+  end
+
+  def decrement_double_quality(item)
+    item.quality -= 2
+    item.quality = 0 if item.quality.negative?
+  end
+
+  def decrement_sell_in(item)
+    item.sell_in -= 1
+    item.sell_in = 0 if item.sell_in.negative?
+  end
+
+  def ten_or_less(item)
+    item.quality += 2
+    item.quality = 50 if item.quality > 50
+  end
+
+  def five_or_less(item)
+    item.quality += 3
+    item.quality = 50 if item.quality > 50
   end
 end
 
@@ -67,50 +99,5 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
-  end
-end
-
-
-
-
-
-
-class Methods
-  attr_accessor :item
-
-  def initialize(item)
-    @item = item
-  end
-
-  def decrement_double_quality
-    item.quality -= 2
-  end
-
-  def increment_quality
-    item.quality += 1
-  end
-
-  def increment_double_quality
-    item.quality += 2
-  end
-
-  def quality_limit
-    item.quality <= 50 ? true : false
-  end
-
-  def ten_or_days_less
-    item.quality += 2     
-  end
-
-  def five_or_less    
-    item.quality += 3
-  end
-
-  def after_sell_in
-    item.quality = 0    
-  end
-
-  def on_sell_limits
-    item.quality > 0 ? true : false
   end
 end
